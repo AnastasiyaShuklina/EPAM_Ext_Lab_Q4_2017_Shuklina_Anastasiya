30933f56e2547a2404584f780ea01bf6a85b5fd5
AnastasiyaShuklina <snow_rat1@mail.ru>
2017-11-26 19:51:49 +0400

    first commit

---
+++ Task01/Program.cs
@@ -0,0 +1,80 @@
+﻿/* Написать консольное приложение, которое проверяет принадлежность точки заштрихованной области.  
+Пользователь вводит координаты точки (x; y) и выбирает букву графика (a-к). 
+В консоли должно высветиться сообщение: «Точка [(x; y)] принадлежит фигуре [г]». */
+
+using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static bool result;
+        static void Main(string[] args)
+        {
+            Console.Write("Введиет x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введиет y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Введите букву графика: ");
+            char figure = char.Parse(Console.ReadLine());
+
+            if (figure == 'а') FigureA(x, y);
+            if (figure == 'б') FigureB(x, y);
+            if (figure == 'в') FigureV(x, y);
+            if (figure == 'г') FigureG(x, y);
+            if (figure == 'д') FigureD(x, y);
+            if (figure == 'е') FigureE(x, y);
+            if (figure == 'ж') FigureZH(x, y);
+            if (figure == 'з') FigureZ(x, y);
+            if (figure == 'и') FigureI(x, y);
+            if (figure == 'к') FigureK(x, y);
+
+            if (result == true) Console.WriteLine("Точка (" + x + "; " + y + ") принадлежит фигуре " + figure);
+            else Console.WriteLine("Точка (" + x + "; " + y + ") не принадлежит фигуре " + figure);
+            Console.ReadKey();
+        }
+
+        static void FigureA(double x, double y)
+        {
+            result = Math.Pow(x, 2) + Math.Pow(y, 2) <= 1;
+        }
+        static void FigureB(double x, double y)
+        {
+            result = ( ((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1) && ((Math.Pow(x, 2) + Math.Pow(y, 2)) >= 0.5) );
+        }
+        static void FigureV(double x, double y)
+        {
+            result = (((Math.Pow(x, 2) <= 1) && (Math.Pow(y, 2) <= 1)));
+        }
+        static void FigureG(double x, double y)
+        {
+            result = Math.Abs(x) + Math.Abs(y) <= 1;
+        }
+        static void FigureD(double x, double y)
+        {
+            result = Math.Abs(x) + 0.5 * Math.Abs(y) <= 0.5;
+        }
+        static void FigureE(double x, double y)
+        {
+            result = ( ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (x > 0)) 
+                || ((Math.Abs(x) + 2 * Math.Abs(y)) <= 2) );
+        }
+        static void FigureZH(double x, double y)
+        {
+            result = ((2 * x + 2 - y >= 0) && (-2 * x + 2 - y >= 0) && (y >= -1));
+        }
+        static void FigureZ(double x, double y)
+        {
+            result = (((Math.Pow(x, 2) <= 1) && (Math.Pow(y, 2) <= 4)) && ((x + y <= 0) || (x - y >= 0)) );
+        }
+        static void FigureI(double x, double y)
+        {
+            result = ((2 * x + 3 - y >= 0) && (-x + 3 * y + 1 >= 0) &&
+             ((x + y <= 0) || (y <= 0)));
+        }
+        static void FigureK(double x, double y)
+        {
+            result = ((y >= 1) || (x + y >= 0) && (x - y <= 0));
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,65 @@
+﻿//Дано действительное число h.
+//Выяснить, имеет ли уравнение ax^2 + bx + c = 0 действительные корни, 
+//если  a = ..., b = ..., c = ...
+//Если действительные корни существуют, то найти их.
+//В противном случае ответом должно служить сообщение,
+//что действительных корней нет.
+//Программа должна выводить пользователю промежуточные вычисления 
+//(например, a, b, c и дискриминант (если вычисляли при помощи него) и корни (если есть)). 
+
+using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите действительное число h: ");
+            double h = double.Parse(Console.ReadLine());
+            FindA(h);
+            Console.ReadKey();
+        }
+        static void FindA(double h)
+        {
+            double a1 = Math.Abs(Math.Sin(8 * h)) + 17;
+            double a2 = 1 - Math.Sin(4 * h) * Math.Cos(Math.Pow(h, 2) + 18);
+            double a3 = Math.Pow(a2, 2);
+            double a = Math.Sqrt(a1 * a3);
+            Console.WriteLine("а = " + a);
+            FindB(h, a);
+        }
+        static void FindB(double h, double a)
+        {
+            double b1 = Math.Abs(Math.Tan(a * Math.Pow(h, 2)) - Math.Sin(a * h));
+            double b = 1 - Math.Sqrt(3 / (3 + b1));
+            Console.WriteLine("b = " + b);
+            FindC(h, a, b);
+        }
+        static void FindC(double h, double a, double b)
+        {
+            double c = a * Math.Pow(h, 2) * Math.Sin(b * h)
+                + b * Math.Pow(h, 3) * Math.Cos(a * h);
+            Console.WriteLine("c = " + c);
+            FindX(a, b, c);
+        }
+        static void FindX(double a, double b, double c)
+        {
+            double discriminant = Math.Pow(b, 2) - 4 * a * c;
+            Console.WriteLine("D = " + discriminant);
+            if (discriminant < 0) Console.WriteLine("\nКорней нет");
+            if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("\nУравнение имеет один корень = " + x);
+            }
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine("\nУравнение имеет два корня: \n" + "х1 = " + x1);
+                Console.WriteLine("х2 = " + x2);
+            }
+        }
+    }
+}
